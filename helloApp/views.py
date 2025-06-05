e497a696ec8940153d3eef530421df6174a0dc06
Ibrahimibu <ibu@Ibrahim.>
2025-06-06 00:11:26 +0600

    add dynamic env generator

--- helloApp/views.py
+++ helloApp/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
 from .getS3Obj import download_s3_object
 from django.http import HttpResponse
+import os
 
 def hello(request):
-  return render(request, 'hello.html', {'message': 'Hello, World!'})
+  msg = os.environ.get('WELCOME_MESSAGE', 'Hello, World!')
+  print(f"Message from environment: {msg}")
+  return render(request, 'hello.html', {'message': msg})
   
   
 def download_file_view(request):
-    bucket = 'test-django-irsa-testing'
-    key = 'ubuntu-logo.png'
-    local_file = '/tmp/file.jpg'
+  bucket = 'test-django-irsa-testing'
+  key = 'ubuntu-logo.png'
+  local_file = '/tmp/file.jpg'
 
-    result = download_s3_object(bucket, key, local_file)
-    if result is True:
-        with open(local_file, 'rb') as f:
-            return HttpResponse(f.read(), content_type='image/jpeg')
-    else:
-        return HttpResponse(f"S3 download failed:\n{result}", status=500)
+  result = download_s3_object(bucket, key, local_file)
+  if result is True:
+      with open(local_file, 'rb') as f:
+          return HttpResponse(f.read(), content_type='image/jpeg')
+  else:
+      return HttpResponse(f"S3 download failed:\n{result}", status=500)
