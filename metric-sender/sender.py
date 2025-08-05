5067f65fb04c2bb8896296706f1e54e4db80541d
root <root@Ibrahim.>
2025-08-06 00:08:01 +0600

    Add metric-sender before-low ECR workflow

---
+++ metric-sender/sender.py
@@ -0,0 +1,20 @@
+from datadog import initialize, statsd
+import time
+import socket
+import os
+
+options = {
+    "statsd_host": os.getenv("DD_AGENT_HOST", "localhost"),
+    "statsd_port": int(os.getenv("DD_DOGSTATSD_PORT", 8125)),
+}
+
+initialize(**options)
+
+while True:
+    statsd.increment("for_ingestion_tags_before", tags=[
+        f"host:{socket.gethostname()}",
+        "env:eks",
+        "app:cardinality-check"
+    ])
+    print("Metric sent")
+    time.sleep(10)
